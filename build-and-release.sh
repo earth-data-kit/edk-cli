2997c88a1eecde8fbdbe590ce42b22477d4283c3
Siddhant Gupta <siddhantgupta3@gmail.com>
2025-10-16 15:32:30 +0530

    bug fixed

--- build-and-release.sh
+++ build-and-release.sh
@@ -2,4 +2,4 @@ TAG=$(poetry version -s)
 poetry build
 
 gh release create $TAG --title $TAG --generate-notes
-gh release upload $TAG dist/edk-cli-$TAG.tar.gz dist/edk-cli-$TAG-py3-none-any.whl
+gh release upload $TAG dist/edk_cli-$TAG.tar.gz dist/edk_cli-$TAG-py3-none-any.whl
